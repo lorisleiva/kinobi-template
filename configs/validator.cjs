01e01f19768ae3d46efbb4c3e3f118ceb32d86ba
Loris Leiva <loris.leiva@gmail.com>
2024-02-26 12:45:28 +0000

    Add more .env variables

--- configs/validator.cjs
+++ configs/validator.cjs
@@ -3,30 +3,24 @@ const path = require("path");
 module.exports = {
   validator: {
     commitment: "processed",
-    programs: [
-      ...getExternalPrograms(),
-      {
-        label: "Counter",
-        programId: "MyProgram1111111111111111111111111111111111",
-        deployPath: getProgram("acme_counter.so"),
-      },
-    ],
+    programs: getAllPrograms(),
   },
 };
 
-function getProgram(programBinary) {
-  return path.join(__dirname, "..", process.env.PROGRAMS_OUTPUT, programBinary);
-}
-
-function getExternalPrograms() {
-  const addresses = process.env.PROGRAMS_EXTERNAL_ADDRESSES.split(/\s+/);
-  const binaries = process.env.PROGRAMS_EXTERNAL_BINARIES.split(/\s+/);
-  return addresses.map((address, index) => {
-    const binary = binaries[index];
+function getAllPrograms() {
+  const addresses = `${process.env.PROGRAMS_ADDRESSES} ${process.env.PROGRAMS_EXTERNAL_ADDRESSES}`;
+  const binaries = `${process.env.PROGRAMS_BINARIES} ${process.env.PROGRAMS_EXTERNAL_BINARIES}`;
+  const binariesArray = binaries.split(/\s+/);
+  return addresses.split(/\s+/).map((address, index) => {
+    const binary = binariesArray[index];
     return {
       label: binary.replace(/\.so$/, ""),
       programId: address,
-      deployPath: getProgram(binary),
+      deployPath: getBinaryPath(binary),
     };
   });
+}
+
+function getBinaryPath(programBinary) {
+  return path.join(__dirname, "..", process.env.PROGRAMS_OUTPUT, programBinary);
 }
