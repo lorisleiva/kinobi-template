ca600cbf4516aedbf37821cee2421d75c577eed8
Loris Leiva <loris.leiva@gmail.com>
2024-02-26 12:58:50 +0000

    Fix Rust client tests

--- clients/rust/tests/create.rs
+++ clients/rust/tests/create.rs
@@ -1,56 +1,47 @@
 #![cfg(feature = "test-sbf")]
 
+use acme_counter_client::{accounts::Counter, instructions::CreateBuilder};
 use borsh::BorshDeserialize;
-use mpl_project_name::{accounts::MyAccount, instructions::CreateBuilder};
 use solana_program_test::{tokio, ProgramTest};
-use solana_sdk::{
-    signature::{Keypair, Signer},
-    transaction::Transaction,
-};
+use solana_sdk::{signature::Signer, transaction::Transaction};
 
 #[tokio::test]
 async fn create() {
-    let mut context = ProgramTest::new("mpl_project_name_program", mpl_project_name::ID, None)
+    let mut context = ProgramTest::new("acme_counter", acme_counter_client::ID, None)
         .start_with_context()
         .await;
 
-    // Given a new keypair.
+    // Given a PDA derived from the payer's public key.
 
-    let address = Keypair::new();
+    let address = Counter::find_pda(&context.payer.pubkey()).0;
 
     let ix = CreateBuilder::new()
-        .address(address.pubkey())
+        .counter(address)
         .authority(context.payer.pubkey())
         .payer(context.payer.pubkey())
-        .arg1(1)
-        .arg2(2)
         .instruction();
 
-    // When we create a new account.
+    // When we create a new counter.
 
     let tx = Transaction::new_signed_with_payer(
         &[ix],
         Some(&context.payer.pubkey()),
-        &[&context.payer, &address],
+        &[&context.payer],
         context.last_blockhash,
     );
     context.banks_client.process_transaction(tx).await.unwrap();
 
     // Then an account was created with the correct data.
 
-    let account = context
-        .banks_client
-        .get_account(address.pubkey())
-        .await
-        .unwrap();
+    let account = context.banks_client.get_account(address).await.unwrap();
 
     assert!(account.is_some());
 
     let account = account.unwrap();
-    assert_eq!(account.data.len(), MyAccount::LEN);
+    assert_eq!(account.data.len(), Counter::LEN);
 
     let mut account_data = account.data.as_ref();
-    let my_account = MyAccount::deserialize(&mut account_data).unwrap();
-    assert_eq!(my_account.data.field1, 1);
-    assert_eq!(my_account.data.field2, 2);
+    let counter = Counter::deserialize(&mut account_data).unwrap();
+    assert_eq!(counter.authority, context.payer.pubkey());
+    assert_eq!(counter.value, 0);
 }
