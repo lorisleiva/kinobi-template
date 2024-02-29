0be50f795fea29b30c83da61e76eebd00b6be4b0
Loris Leiva <loris.leiva@gmail.com>
2024-02-29 11:08:23 +0000

    Start local validator before running client tests

--- scripts/client/test-js.sh
+++ scripts/client/test-js.sh
@@ -4,8 +4,9 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source $(dirname $SCRIPT_DIR)/utils.sh
 
-# Go to the JS client directory.
-cd ${WORKING_DIR}/clients/js
+# Start the local validator.
+(cd ${WORKING_DIR} && pnpm validator)
 
 # Build the client and run the tests.
+cd ${WORKING_DIR}/clients/js
 pnpm install && pnpm build && pnpm test $*
--- scripts/client/test-rust.sh
+++ scripts/client/test-rust.sh
@@ -4,10 +4,11 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source $(dirname $SCRIPT_DIR)/utils.sh
 
-# Go to the Rust client directory.
-cd ${WORKING_DIR}/clients/rust
+# Start the local validator.
+(cd ${WORKING_DIR} && pnpm validator)
 
 # Run the tests.
+cd ${WORKING_DIR}/clients/rust
 if [ ! "$(command -v $SOL_FORMATTER)" = "" ]; then
     CARGO_TERM_COLOR=always cargo test-sbf $* 2>&1 | ${SOL_FORMATTER}
 else
